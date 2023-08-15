6af490ef86a4bdd4b14429942b33c7ecfc2d3e53
yousef <ywsfalshry236@gmail.com>
2023-08-16 00:15:24 +0300

    Edite In A Css Fill And Js Fill

--- imog.js
+++ imog.js
@@ -1,8 +1,12 @@
+let feelIcon=document.querySelector(".post .tolls .feel")
 let fell=document.querySelector(".post .felling > span:first-child")
 let whatFell=document.querySelector(".post .felling > span:last-child")
 let felling=document.querySelector(".post .felling")
+let local=document.querySelector(".post .tolls .location")
+let user=document.querySelector(".post .tolls .user")
 
-//Create Objects 
+
+//images Objects 
 let images={
     1:{
     Name:"السعادة",
@@ -45,20 +49,95 @@ let images={
     baseUrl:"fa-solid fa-face-grin-stars"
 }
 }
+// loction Object
+let locations={
+    1:{
+        Name:"القاهرة"
+    },
+    2:{
+        Name:"الاسكندرية"
 
+    }, 
+    3:{
+        Name:"طنطا"
 
-whatFell.onclick=function(){
+    }, 
+    4:{
+        Name:"البحيرة"
+
+    }, 
+    5:{
+        Name:"اسوان"
+
+    },
+    6:{
+        Name:"الجيزة"
+
+    },7:{
+        Name:"الغردقة"
+
+    },8:{
+        Name:"شرم الشيخ"
+
+    }
+
+}
+// User Object
+let users={
+    1:{
+        Name:"mohamed Ahmed",
+        avter:"team-05.png"
+    },
+    2:{
+        Name:"yousef Ahmed",
+        avter:"friend-01.jpg"
+
+    }, 
+    3:{
+        Name:"Ahmed Ali",
+        avter:"friend-02.jpg"
+
+    }, 
+    4:{
+        Name:"Ali Mohamed",
+        avter:"friend-03.jpg"
+
+    }, 
+    5:{
+        Name:"Ibrhem Ahmed",
+        avter:"friend-04.jpg"
+
+    },
+    6:{
+        Name:"mohmoud Hassan",
+        avter:"friend-05.jpg"
+
+    },7:{
+        Name:"Hassan Ahmed",
+        avter:"team-01.png"
+
+    },8:{
+        Name:"yasen Ali",
+        avter:"team-03.png"
+
+    }
+
+}
+
+// feeling click
+feelIcon.onclick=function(){
     fell.textContent="اشعر ب"
+    whatFell.textContent="بم تشعر؟"
     fell.style="background-color:#3658983d"
     createOption()
 }
 
-//Function To  Create The Element 
+//Function To  Create The Element To Feeling Object
 function createOption(){
-    let select=document.createElement("div")
+    let select=document.createElement("ul")
     select.className="images"
-    for(let i = 1;i<=10;i++){
-        let option=document.createElement("div")
+    for(let i = 1;i<=Object.keys(images).length;i++){
+        let option=document.createElement("li")
         let icon=document.createElement("i")
         icon.className=images[i].baseUrl
         let status=document.createElement("p")
@@ -74,4 +153,66 @@ function createOption(){
     }
     felling.appendChild(select)
     
+}
+//Function To  Create The Element To Contry
+
+function Contry(){
+    let select=document.createElement("ul")
+    select.className="images"
+    for(let i = 1;i<=Object.keys(locations).length;i++){
+        let option=document.createElement("li")
+        let status=document.createElement("p")
+        status.textContent=locations[i].Name
+        option.appendChild(status)
+        select.appendChild(option)
+
+        option.onclick=function(){
+            
+            fell.textContent=status.innerHTML
+            select.remove()
+        }
+    }
+    felling.appendChild(select)
+    
+}
+
+
+// location click
+
+local.onclick=function(){
+    whatFell.textContent=""
+    fell.textContent=""
+
+    Contry()
+}
+
+
+//Function To  Create The Element To users
+
+function theUsers(){
+    let select=document.createElement("ul")
+    select.className="images"
+    for(let i = 1;i<=Object.keys(users).length;i++){
+        let option=document.createElement("li")
+        let img=document.createElement("img")
+        img.src=`img/${users[i].avter}`
+        let status=document.createElement("p")
+        status.textContent=users[i].Name
+        option.appendChild(img)
+        option.appendChild(status)
+        select.appendChild(option)
+
+        option.onclick=function(){
+            
+            fell.textContent=status.innerHTML
+            select.remove()
+        }
+    }
+    felling.appendChild(select)
+    
+}
+user.onclick=function(){
+    whatFell.textContent=""
+    fell.textContent=""
+    theUsers()
 }
